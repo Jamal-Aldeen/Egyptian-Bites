32e65bb7eb98f9f99da9b6121caa8a63ea3e140f
Essam-sara <saraessam2580@gmail.com>
2025-02-08 03:10:44 +0200

    remove a unusable line that appear at index page

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -10,9 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/public/css/styles.css">
 
-
-
-</head>/public/assets/style.css
+</head>
 <body>
 
 <!-- Navbar -->
