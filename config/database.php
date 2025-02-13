293112a8204ad789b4b050a3f8c3ad2140e8c9ba
Essam-sara <saraessam2580@gmail.com>
2025-02-13 20:02:56 +0200

    fix issue restaurant_db.sql from version control and add database.php to .gitignore

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost:3306');
 define('DB_NAME', 'restaurant_db');
-define('DB_USER', 'restaurant');
-define('DB_PASSWORD', '3306passwd');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 ?>
