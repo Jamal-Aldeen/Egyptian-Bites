78ab74174bc379425a52f3bc41629255764fa879
Essam-sara <saraessam2580@gmail.com>
2025-02-15 13:49:49 +0200

    Refine Header Navigation
    # Changes made:
    # - Improved navbar alignment for a cleaner design.
    Fixed potential inconsistencies in header layout.

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -14,39 +14,44 @@
 <body>
 
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg">
     <div class="container">
-        <a class="navbar-brand" href="#">Egyptian Bites</a>
+        <!-- Brand Logo -->
+        <a class="navbar-brand" href="#"><i class="fas fa-utensils"></i> Egyptian Bites</a>
+
+        <!-- Mobile Toggle Button -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
+            <span class="fas fa-bars"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                
-                <li class="nav-item"><a class="nav-link active" href="#home" >Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="#menu">Menu</a></li>
-                <li class="nav-item"><a class="nav-link" href="#booking">Book a Table</a></li>
-                <!-- <li class="nav-item"><a class="nav-link" href="#testimonials">Testimonials</a></li> -->
-                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                <!-- Add to header navigation -->
-<?php if (isset($_SESSION['user_id'])) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="/views/customer/profile.php">My Profile</a>
-    </li>
-    <li class="nav-item">
-<a href="/logout" class="nav-link">Logout</a>
-    </li>
-<?php else : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="/views/shared/login.php">Login</a>
-        <li class="nav-item"><a class="nav-link" href="/views/shared/register-form.php">Register</a></li>
-    </li>
-<?php endif; ?>
+            <!-- Centered Navigation Links -->
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item on"><a class="nav-link " href="#home">Home</a></li>
+                <li class="nav-item on"><a class="nav-link" href="#about">About</a></li>
+                <li class="nav-item on"><a class="nav-link" href="#menu">Menu</a></li>
+                <li class="nav-item on"><a class="nav-link" href="#booking">Book a Table</a></li>
+            </ul>
+
+            <!-- Right-Aligned Login/Register -->
+            <ul class="navbar-nav">
+                <?php if (isset($_SESSION['user_id'])) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-outline-warning px-3" href="/logout">Logout</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-outline-warning px-3 mx-2" href="/views/shared/login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-warning px-3" href="/views/shared/register-form.php">Register</a>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
 </nav>
+
 
 <!-- JavaScript to Close Navbar After Click -->
 <script>
@@ -63,6 +68,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 </script>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
