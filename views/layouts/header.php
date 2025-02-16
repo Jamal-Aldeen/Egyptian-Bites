03d05a9d50fbb390e37fd84af3356ca29483bd07
Essam-sara <saraessam2580@gmail.com>
2025-02-16 08:11:15 +0200

    Updated header layout

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/public/css/styles.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
 </head>
 <body>
 
@@ -21,7 +23,7 @@
 
         <!-- Mobile Toggle Button -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="fas fa-bars"></span>
+            
             
         </button>
 
