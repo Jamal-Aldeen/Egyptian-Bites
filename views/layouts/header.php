27b48527bd189c33e6ae3a9946ba15ec192c2ec5
zina125 <zina.ragab12@gmail.com>
2025-02-16 11:30:21 +0200

    Added order cart icon to Navbar

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="/public/css/styles.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
 </head>
 <body>
 
@@ -22,9 +21,7 @@
         <a class="navbar-brand" href="../../index.php"><i class="fas fa-utensils"></i> Egyptian Bites</a>
 
         <!-- Mobile Toggle Button -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            
-            
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">   
         </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -36,8 +33,18 @@
                 <li class="nav-item on"><a class="nav-link" href="#booking">Book a Table</a></li>
             </ul>
 
-            <!-- Right-Aligned Login/Register -->
+            <!-- Right-Aligned Login/Register & Cart -->
             <ul class="navbar-nav">
+                <!-- Order Cart Icon -->
+                <li class="nav-item">
+                    <a class="nav-link position-relative" href="/views/customer/order-placement.php">
+                        <div class="cart-card">
+                            <i class="fa-solid fa-shopping-cart fa-lg"></i>
+                            <span id="cart-count" class="badge bg-danger position-absolute top-0 start-100 translate-middle">0</span>
+                        </div>
+                    </a>
+                </li>
+
                 <?php if (isset($_SESSION['user_id'])) : ?>
                     <li class="nav-item">
                         <a class="nav-link btn btn-outline-warning px-3" href="/logout">Logout</a>
@@ -55,7 +62,6 @@
     </div>
 </nav>
 
-
 <!-- JavaScript to Close Navbar After Click -->
 <script>
 document.addEventListener("DOMContentLoaded", function () {
@@ -69,9 +75,15 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-});
-</script>
 
+    updateCartCount();
+});
+
+function updateCartCount() {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    document.getElementById("cart-count").innerText = cart.length;
+}
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
