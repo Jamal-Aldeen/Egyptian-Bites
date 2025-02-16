ef6cbe5b82c5c417cd7a1c3c5c1c134ba9c95f24
jamal-aldeen <jamal.aldeen.y@gmail.com>
2025-02-16 20:11:12 +0200

    Added TCPDF library in lib folder

---
+++ lib/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '618091e0c3e74b25afa2252b65ad71b30663801f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '618091e0c3e74b25afa2252b65ad71b30663801f',
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.8.2',
+            'version' => '6.8.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'reference' => 'f7a781073e1645062f163e058139e2f89355d420',
+            'dev_requirement' => false,
+        ),
+    ),
+);
