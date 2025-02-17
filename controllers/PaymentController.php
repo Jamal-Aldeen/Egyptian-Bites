44adbd1702b02b0c8869f578acf01dc448f4688b
zina125 <zina.ragab12@gmail.com>
2025-02-18 00:13:08 +0200

    Modified controllers/PaymentController.php to handle payment gateway

--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/stripe.php';
 require_once __DIR__ . '/../models/Payment.php';
-require_once __DIR__ . '/../vendor/autoload.php'; 
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 class PaymentController {
     private $paymentModel;
