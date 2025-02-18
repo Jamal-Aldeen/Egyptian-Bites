a4b769e3889a77799063a0bb6f5e3686729287cb
ahmed332 <ahmedmonsef787@gmail.com>
2025-02-18 12:41:54 +0200

    Update sidebar.php styles and links

---
+++ views/layouts/sidebar.php
@@ -0,0 +1,38 @@
+
+<nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-dark text-white sidebar">
+                <div class="position-sticky">
+                    <h5 class="sidebar-heading text-light">Admin Tools</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" href="user-management.php">
+                                <i class="fas fa-users"></i> Manage Users
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="menu_items.php">
+                                <i class="fas fa-utensils"></i> Manage items
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="menu-management.php">
+                                <i class="fas fa-utensils"></i> Manage Menu
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="reservations.php">
+                                <i class="fas fa-utensils"></i> reservations
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="inventory-management.php">
+                                <i class="fas fa-boxes"></i> Manage Inventory
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="reports.php">
+                                <i class="fas fa-chart-bar"></i> Sales Reports
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
