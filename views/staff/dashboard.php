6cd04e0d2ccaf5f90671d4fdd00ed04b856a58ca
ahmed332 <ahmedmonsef787@gmail.com>
2025-02-18 12:46:31 +0200

    add edit on dashbord

--- views/staff/dashboard.php
+++ views/staff/dashboard.php
@@ -97,38 +97,9 @@ $upcomingReservations = $reservationModel->getUpcomingReservationCount();
 
     <div class="container-fluid mt-4">
         <div class="row">
-            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-dark text-white sidebar">
-                <div class="position-sticky">
-                    <h5 class="sidebar-heading text-light">Admin Tools</h5>
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active text-white" href="user-management.php">
-                                <i class="fas fa-users"></i> Manage Users
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="menu-management.php">
-                                <i class="fas fa-utensils"></i> Manage Menu
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="reservations.php">
-                                <i class="fas fa-utensils"></i> reservations
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="inventory-management.php">
-                                <i class="fas fa-boxes"></i> Manage Inventory
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="reports.php">
-                                <i class="fas fa-chart-bar"></i> Sales Reports
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+         <?php
+         require_once "../layouts/sidebar.php";
+         ?>
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-4">
                 <h1 class="text-center mb-4 text-dark">Staff Dashboard</h1>
