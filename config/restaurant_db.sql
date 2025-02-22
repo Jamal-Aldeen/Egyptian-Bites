b82f5b5847287b0dad950b74069d0a057c273cad
Essam-sara <saraessam2580@gmail.com>
2025-02-22 08:21:04 +0200

    add notification table

--- config/restaurant_db.sql
+++ config/restaurant_db.sql
@@ -393,3 +393,12 @@ CREATE TABLE Payments (
     FOREIGN KEY (user_id) REFERENCES Users(id),
     FOREIGN KEY (order_id) REFERENCES Orders(id)
 );
+
+CREATE TABLE notifications (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
