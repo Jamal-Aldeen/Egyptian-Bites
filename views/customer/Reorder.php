6c2ae83ab9c3a51de97dcfb027d75e009ff7e6da
zina125 <zina.ragab12@gmail.com>
2025-03-01 22:12:37 +0200

    Added reorder functionality improvements

--- views/customer/Reorder.php
+++ views/customer/Reorder.php
@@ -14,20 +14,19 @@ if (!$orderDetails) {
     echo "Invalid order items.";
     exit();
 }
-
 $_SESSION['cart'] = [];
 foreach ($orderDetails['items'] as $item) {
     $_SESSION['cart'][] = [
-        'id' => $item['menu_item_id'], // تأكد من أن هذا هو المفتاح الصحيح
-        'name' => $item['product_name'],
+        'id' => $item['menu_item_id'],
+        'name' => $item['name'], 
         'price' => $item['price'],
-        'quantity' => $item['quantity'],
-        'category_id' => $item['category_id'],
+        'quantity' => $item['quantity']
     ];
 }
 
 header("Location: /views/customer/cart.php?Reorder=success");
 exit();
+
 ?>
 <?php include '../layouts/header.php'; ?>
 
